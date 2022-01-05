20339bdf5fcc2b4a244e114a03cc502c48b0f064
lucasfer01 <lucasfer9912@gmail.com>
2022-01-05 14:02:28 -0300

    Enviroment variable config for Postgres

--- src/config.js
+++ src/config.js
@@ -2,5 +2,8 @@ const dotenv = require('dotenv').config();
 
 module.exports = {
     PORT: process.env.PORT || 3000,
-    MONGO_URL: process.env.MONGO_URL,
+    POSTGRES_USER: process.env.POSTGRES_USER,
+    POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+    POSTGRES_HOST: process.env.POSTGRES_HOST,
+    POSTGRES_DB_NAME: process.env.POSTGRES_DB_NAME
 }
