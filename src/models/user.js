5d5ece961dde28e880c6a91f2f065d865a14c2c5
lucasfer01 <lucasfer9912@gmail.com>
2022-01-06 11:47:44 -0300

    Exports model function

--- src/models/user.js
+++ src/models/user.js
@@ -1,43 +1,43 @@
+// Tipo de datos
 const { DataTypes } = require('sequelize');
-const sequelize = require('../database/db');
 
 // Definimos el modelo
-const userModel = sequelize.define('User', {
-    usr_id: {
-        type: DataTypes.UUID,
-        primaryKey: true,
-        defaultValue: DataTypes.UUIDV4
-    },
-    usr_username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    usr_password: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    usr_email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            isEmail: true
+module.exports = (sequelize) => {
+    sequelize.define('user', {
+        usr_id: {
+            type: DataTypes.UUID,
+            primaryKey: true,
+            defaultValue: DataTypes.UUIDV4
+        },
+        usr_username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        usr_password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        usr_email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true
+            }
+        },
+        usr_photo: {
+            type: DataTypes.STRING
+        },
+        usr_role: {
+            type: DataTypes.ENUM('user', 'admin')
+        },
+        usr_description: {
+            type: DataTypes.STRING
+        },
+        usr_location: {
+            type: DataTypes.JSON // Google map!!
+        },
+        usr_score: {
+            type: DataTypes.INTEGER
         }
-    },
-    usr_photo: {
-        type: DataTypes.STRING
-    },
-    usr_role: {
-        type: DataTypes.ENUM('user', 'admin')
-    },
-    usr_description: {
-        type: DataTypes.STRING
-    },
-    usr_location: {
-        type: DataTypes.JSON // Google map!!
-    },
-    usr_score: {
-        type: DataTypes.INTEGER
-    }
-});
-
-module.exports = userModel;
+    });
+}
