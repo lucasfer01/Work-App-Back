5811ec57675c0c4a34adb9b5073bdac9dab69e5f
Martin Morlio <tincho20012017@gmail.com>
2022-01-06 15:09:49 -0300

    nose

--- src/database/db.js
+++ src/database/db.js
@@ -1,9 +1,12 @@
 // Sequelize
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 // Enviroment
-const config = require('../config');
+const config = require("../config");
 
 // PostgreSQL
-const sequelize = new Sequelize(`postgres://${config.POSTGRES_USER}:${config.POSTGRES_PASSWORD}@${config.POSTGRES_HOST}:5432/${config.POSTGRES_DB_NAME}`, {logging: false});
+const sequelize = new Sequelize(
+  `postgres://${config.POSTGRES_USER}:${config.POSTGRES_PASSWORD}@${config.POSTGRES_HOST}:5432/${config.POSTGRES_DB_NAME}`,
+  { logging: false }
+);
 
 module.exports = sequelize;
