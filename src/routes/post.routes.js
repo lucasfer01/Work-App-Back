57b21b4268a5cb58710e4f538d85d91e0ee45a3d
lucasfer01 <lucasfer9912@gmail.com>
2022-01-08 13:29:28 -0300

    CRUD post Completed

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,7 +1,11 @@
 // Router Express
-const {Router} = require('express');
+const { Router } = require('express');
 // Post controllers
-const {createPost, showPosts, showPostById} = require('../controllers/post.controller');
+const { createPost,
+        showPosts, 
+        showPostById, 
+        modifyPost, 
+        deletePost } = require('../controllers/post.controller');
 
 // Creamos router
 const postRouter = Router();
@@ -12,6 +16,10 @@ postRouter.get('/', showPosts); // Mostramos todos los posts
 postRouter.get('/:postId', showPostById); // Mostramos post por id
 
 postRouter.post('/', createPost); // Creamos un post
+
+postRouter.put('/:postId', modifyPost); // Modificamos el post
+
+postRouter.delete('/:postId', deletePost); // Borramos el post
 
 // Exportamos router
 module.exports = {
