89e600e721d1714e15c41f669feb6d7db530415d
Jehison <jehison-98@hotmail.com>
2022-01-12 09:09:37 -0600

    back to previous commit

--- src/models/user.js
+++ src/models/user.js
@@ -3,49 +3,6 @@ const { DataTypes } = require("sequelize");
 
 // Definimos el modelo
 module.exports = (sequelize) => {
-<<<<<<< HEAD
-  sequelize.define("user", {
-    usr_id: {
-      /* type: DataTypes.INTEGER, */
-      type: DataTypes.STRING,
-      primaryKey: true,
-      /* autoIncrement: true */
-      unique: true,
-      allowNull: false,
-    },
-    usr_username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    /*     usr_password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    }, */
-    usr_email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        isEmail: true,
-      },
-    },
-    usr_photo: {
-      type: DataTypes.STRING,
-    },
-    usr_role: {
-      type: DataTypes.ENUM("user", "admin"),
-    },
-    usr_description: {
-      type: DataTypes.STRING,
-    },
-    usr_location: {
-      type: DataTypes.JSON, // Google map!!
-    },
-    usr_score: {
-      type: DataTypes.INTEGER,
-    },
-  });
-};
-=======
     sequelize.define('user', {
         usr_id : {
             type: DataTypes.STRING,
@@ -82,4 +39,3 @@ module.exports = (sequelize) => {
         }
     });
 }
->>>>>>> 79c7acf4e56aad93d9133a80a1dee99f0f0e9989
