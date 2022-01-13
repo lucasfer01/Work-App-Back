e21e824d8b245d67fc58cc07b4b634455bcf503b
lucasfer01 <lucasfer9912@gmail.com>
2022-01-13 12:20:20 -0300

    Pasarela de pago incorporada

---
+++ src/paymentGateway/index.js
@@ -0,0 +1,40 @@
+// Requerimos mercadopago
+const mercadopago = require('mercadopago');
+// Enviroment
+const { MP_ACCESS_TOKEN, MP_FAILURE_URL, MP_SUCCESS_URL } = require('../config');
+
+
+// Access token
+mercadopago.configure({
+    access_token: MP_ACCESS_TOKEN
+})
+
+// checkout
+const checkout = (req, res, next) => {
+    // preference data
+    const dataPreference = req.body;
+
+    // preferencia
+    const preference = {
+        items: [{
+            ...dataPreference
+        }], 
+        back_urls: {
+            success: MP_SUCCESS_URL,
+            failure: MP_FAILURE_URL,
+            pending: ''
+        }
+    }
+
+    // Crear preferencia
+    mercadopago.preferences.create(preference)
+        .then(response => {
+            // Crear preferencia y retornarla
+            res.json(response.body);
+        })
+        .catch(error => next(error));
+}
+
+module.exports = {
+    checkout
+}
